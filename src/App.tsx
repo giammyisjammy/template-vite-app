2c714d71c35667b6b350630b9d15d9738782807e
gbado <gbado@intesigroup.com>
2024-06-13 14:51:08 +0200

    💡 Add note in comment

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,30 @@
-import { Counter } from '@/components/Counter';
+import { Counter } from '@/components/Counter'
 
-import reactLogo from './assets/react.svg';
+import reactLogo from './assets/react.svg'
 
-import viteLogo from '/vite.svg';
-import './App.css';
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+        {/* This should raise a linter error with production eslint config */}
+        {/* <a href="https://vitejs.dev" target="_blank"> */}
+        <a href='https://vitejs.dev' target='_blank'>
+          <img src={viteLogo} className='logo' alt='Vite logo' />
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a href='https://react.dev' target='_blank'>
+          <img src={reactLogo} className='logo react' alt='React logo' />
         </a>
       </div>
       <h1>Vite + React</h1>
       <Counter />
-      <p className="read-the-docs">
+      <p className='read-the-docs'>
         Click on the Vite and React logos to learn more
       </p>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
