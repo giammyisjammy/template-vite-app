9dedd3dfceda2d6a9e34a2f7d4f28b7de9a756cb
gbado <gbado@intesigroup.com>
2024-07-18 15:34:03 +0200

    🏗️ Set up routing library

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 import viteTsconfigPaths from 'vite-tsconfig-paths'
+import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), viteTsconfigPaths()],
+  plugins: [TanStackRouterVite(), react(), viteTsconfigPaths()],
 })
